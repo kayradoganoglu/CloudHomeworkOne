19895c1e202b1bf74cbca3140df47a4a9532ebd9
Kayra Doganoglu <kayradoganoglu2003@gmail.com>
2026-03-10 22:50:05 +0300

    Add files via upload

---
+++ Ascending.py
@@ -0,0 +1,7 @@
+# Take array elements from user and sort in descending order
+
+numbers = list(map(int, input("Enter the array elements separated by spaces: ").split()))
+
+numbers.sort(reverse=True)
+
+print("Sorted array in descending order:", numbers)
